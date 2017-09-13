5cdb305cab57b0992f6f7f302ffb6920a7cea6e4
Kyle Bremner <kyle@defining.tech>
2017-09-13 09:44:28 +0100

    Renames parameter to w instead of window to prevent masking of global window object

--- src/tabs/ChromeTabManager.ts
+++ src/tabs/ChromeTabManager.ts
@@ -48,7 +48,7 @@ class ChromeTabManager implements ITabManager {
             const getInfo = {
                 windowTypes: ["normal"],
             } as chrome.windows.GetInfo;
-            chrome.windows.getLastFocused(getInfo, (window) => handleLastError(resolve, reject, window));
+            chrome.windows.getLastFocused(getInfo, (w) => handleLastError(resolve, reject, w));
         });
     }
 
