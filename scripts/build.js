d18cc87b6869f274d544934c262a5b24eef7e0d3
Kyle Bremner <kyle@defining.tech>
2017-09-18 16:35:36 +0100

    Takes version from package.json and build number, scripts use yarn instead of npm, adds publish script

--- scripts/build.js
+++ scripts/build.js
@@ -1,39 +1,41 @@
-const fs = require("fs");
-const browserify = require("browserify");
-const manifest = require("../assets/manifest.json");
+const fs = require('fs');
+const browserify = require('browserify');
+const manifest = require('../assets/manifest.json');
 const copyfiles = require('copyfiles');
+const packageJson = require('../package.json');
 
-const logglyKey = process.env.LOGGLY_KEY || "";
-const logglyTag = process.env.LOGGLY_TAG || "readmore-extension";
-const baseUri = process.env.BASE_URI || "https://read-more-api.herokuapp.com";
+const baseUri = process.env.BASE_URI || 'https://read-more-api.herokuapp.com';
+const buildNumber = process.env.BUILD_NUMBER || '0';
 
-manifest.permissions.push(baseUri + "/");
-fs.writeFileSync("./dist/manifest.json", JSON.stringify(manifest));
+manifest.version = `${packageJson.version}.${buildNumber}`;
+manifest.permissions.push(baseUri + '/');
+fs.writeFileSync('./dist/manifest.json', JSON.stringify(manifest));
 
-fs.mkdirSync("./dist/scripts");
+fs.mkdirSync('./dist/scripts');
 buildModule('background');
 buildModule('popup');
 
-
-copyfiles(["assets/html/**", "dist/html"], true, function(err, files) {
+copyfiles(['assets/html/**', 'dist/html'], true, function(err, files) {
     if (err) throw err;
 });
 
-copyfiles(["assets/img/**", "dist/img"], true, function(err, files) {
+copyfiles(['assets/img/**', 'dist/img'], true, function(err, files) {
     if (err) throw err;
 });
 
-copyfiles(["assets/style/**", "dist/style"], true, function(err, files) {
+copyfiles(['assets/style/**', 'dist/style'], true, function(err, files) {
+    if (err) throw err;
+});
+
+copyfiles(['assets/*.pem', 'dist'], true, function(err, files) {
     if (err) throw err;
 });
 
 function buildModule(moduleName) {
-  browserify(`./tmp/js/${moduleName}.js`)
-    .transform("browserify-replace", {replace: [
-      { from: /\$logglyKey/, to: logglyKey },
-      { from: /\$logglyTag/, to: logglyTag },
-      { from: /\$baseUri/, to: baseUri }
-    ]})
-    .bundle()
-    .pipe(fs.createWriteStream(`./dist/scripts/${moduleName}.js`));
+    browserify(`./tmp/js/${moduleName}.js`)
+        .transform('browserify-replace', {
+            replace: [{ from: /\$baseUri/, to: baseUri }]
+        })
+        .bundle()
+        .pipe(fs.createWriteStream(`./dist/scripts/${moduleName}.js`));
 }
---
+++ scripts/publish.js
@@ -0,0 +1,87 @@
+const fs = require('fs');
+const zipFolder = require('zip-folder');
+const https = require('https');
+var querystring = require('querystring');
+
+const {
+    PUBLISH_EXTENSION_ID,
+    PUBLISH_CLIENT_ID,
+    PUBLISH_CLIENT_SECRET,
+    PUBLISH_REFRESH_TOKEN
+} = process.env;
+
+const distDir = './dist';
+const distZip = './tmp/dist.zip';
+
+zip(distDir, distZip)
+    .then(() =>
+        request(
+            'POST',
+            '/oauth2/v4/token',
+            'application/x-www-form-urlencoded',
+            querystring.stringify({
+                client_id: PUBLISH_CLIENT_ID,
+                client_secret: PUBLISH_CLIENT_SECRET,
+                refresh_token: PUBLISH_REFRESH_TOKEN,
+                grant_type: 'refresh_token'
+            })
+        )
+    )
+    .then(({ access_token }) =>
+        request(
+            'PUT',
+            `/upload/chromewebstore/v1.1/items/${PUBLISH_EXTENSION_ID}`,
+            'application/octet-stream',
+            fs.readFileSync(distZip),
+            { Authorization: `Bearer ${access_token}` }
+        )
+    )
+    .then(result => console.log('Upload successful!', result))
+    .catch(err => console.error('Upload failed', err));
+
+function zip(folderPath, destPath) {
+    return new Promise((resolve, reject) => {
+        zipFolder(folderPath, destPath, function(err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+function request(method, path, contentType, payload, headers = {}) {
+    return new Promise((resolve, reject) => {
+        if (payload) {
+            headers['Content-Type'] = contentType;
+            headers['Content-Length'] = Buffer.byteLength(payload);
+        }
+
+        const req = https.request(
+            { method, hostname: 'www.googleapis.com', path, headers },
+            resp => {
+                let data = '';
+                resp.on('data', chunk => {
+                    data += chunk;
+                });
+                resp.on('end', () => {
+                    const json = JSON.parse(data);
+                    if (json.error) {
+                        reject(json);
+                    } else {
+                        resolve(json);
+                    }
+                });
+            }
+        );
+
+        req.on('error', reject);
+
+        if (payload) {
+            req.end(payload);
+        } else {
+            req.end();
+        }
+    });
+}
